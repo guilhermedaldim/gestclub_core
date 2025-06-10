a11b8d1a4b2c2bcb2f2b0d3a54aad0e40474e297
Guilherme Daldim <gdaldims@gmail.com>
2025-06-10 18:59:21 -0300

    AuthBasePage - adicionado layout básico

--- lib/features/auth/presentation/pages/auth_base_page.dart
+++ lib/features/auth/presentation/pages/auth_base_page.dart
@@ -2,18 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:gestclub_design_system/gestclub_design_system.dart';
 
 class AuthBasePage extends StatelessWidget {
-  final Widget? child;
-  final String? title;
+  final Widget image;
+  final Widget? forgetPassword;
+  final GestClubInput emailInput;
+  final GestClubInput passwordInput;
+  final GestClubButton button;
 
-  const AuthBasePage({super.key, required this.child, this.title});
+  const AuthBasePage({
+    super.key,
+    required this.image,
+    this.forgetPassword,
+    required this.emailInput,
+    required this.passwordInput,
+    required this.button,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: title != null ? AppBar(title: Text(title!)) : null,
       body: Padding(
         padding: const EdgeInsets.all(GestClubSizes.twentyFour),
-        child: child,
+        child: Column(
+          spacing: GestClubSizes.sixteen,
+          children: [
+            image,
+            emailInput,
+            passwordInput,
+            if (forgetPassword != null) ...[
+              Align(alignment: Alignment.centerRight, child: forgetPassword!),
+            ],
+            button,
+          ],
+        ),
       ),
     );
   }
