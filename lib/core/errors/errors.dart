760caf5e556a26e889877d8b6f5a936df3ffd883
Guilherme Daldim <gdaldims@gmail.com>
2025-06-24 19:34:43 -0300

    Melhorias em AuthFailure e adicionado FirebaseFailure

--- lib/core/errors/errors.dart
+++ lib/core/errors/errors.dart
@@ -1,5 +1,6 @@
 export 'appointments/appointments.dart';
 export 'auth/auth.dart';
 export 'events/events.dart';
-export 'failure.dart';
+export 'firebase/firebase.dart';
 export 'spaces/spaces.dart';
+export 'failure.dart';
