dc0838aaf9261f5255978bacaa8ff961ecd29152
Guilherme Daldim <gdaldims@gmail.com>
2025-09-04 17:00:13 -0300

    Ajuste para métodos genéricos para o StorageServices

--- lib/core/services/storage/storage_services.dart
+++ lib/core/services/storage/storage_services.dart
@@ -4,16 +4,24 @@ import 'package:dartz/dartz.dart';
 import 'package:gestclub_core/gestclub_core.dart';
 
 abstract class StorageServices {
-  Future<Either<Failure, String>> uploadClubLogo({required File file});
-  Future<Either<Failure, void>> deleteClubLogo({required String publicUrl});
-  Future<Either<Failure, String>> updateClubLogo({
-    required String clubId,
-    required File newLogo,
+  Future<Either<Failure, String>> uploadFile({
+    required File file,
+    required String bucket,
+    required String folder,
+    required Failure Function(String) failureBuilder,
   });
-  Future<Either<Failure, String>> uploadEventImage({required File file});
-  Future<Either<Failure, void>> deleteEventImage({required String publicUrl});
-  Future<Either<Failure, String>> updateEventImage({
-    required String eventId,
-    required File newImage,
+  Future<Either<Failure, String>> updateFile({
+    required String id,
+    required File newFile,
+    required String bucket,
+    required String folder,
+    required String table,
+    required String column, // ex: 'image', 'logo_url'
+    required Failure Function(String) failureBuilder,
+  });
+  Future<Either<Failure, void>> deleteFile({
+    required String publicUrl,
+    required String bucket,
+    required Failure Function(String) failureBuilder,
   });
 }
